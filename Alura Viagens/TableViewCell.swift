84291c87671c15d7de24b9e249db2ce500068373
Ândriu Coelho <andriu_coelho1993@hotmail.com>
2017-08-29 11:06:06 -0300

    Aula 2

---
+++ Alura Viagens/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  Alura Viagens
+//
+//  Created by Alura on 17/08/17.
+//  Copyright © 2017 Alura. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+
+}
